0a0865afa0fefb54c6de6cccdcc98b938eeb3a81
jaehuian <jaehuian59@gmail.com>
2022-09-17 23:50:24 +0900

    Backjoon_25083

---
+++ src/Backjoon_25083.java
@@ -0,0 +1,11 @@
+public class Backjoon_25083 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(
+        "         ,r'\"7\n" +
+        "r`-_   ,'  ,/\n" +
+        " \\. \". L_r'\n" +
+        "   `~\\/\n" +
+        "      |\n" +
+        "      |\n");
+    }
+}
