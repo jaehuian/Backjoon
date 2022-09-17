c83a2f922025626f534ad391e2c5feef6f844c10
jaehuian <jaehuian59@gmail.com>
2022-09-17 22:02:49 +0900

    Backjoon example18108

---
+++ src/Backjoon_18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Backjoon_18108 {
+    public static void main(String[] args) throws Exception {
+        Scanner year = new Scanner(System.in);
+
+        System.out.println(year.nextInt()-543);
+
+        year.close();
+    }
+}
