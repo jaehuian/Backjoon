79c3f40926453301520615af7c58812240f601c2
jaehuian <jaehuian59@gmail.com>
2022-09-19 23:03:09 +0900

    Backjoon_10951

---
+++ src/Backjoon_10951.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Backjoon_10951 {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()){
+            System.out.println(sc.nextInt()+sc.nextInt());
+        }
+        sc.close();
+    }
+}
