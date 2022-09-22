eab44f7e0c13ebf468c995748e7d0fcbbd0654ca
jaehuian <jaehuian59@gmail.com>
2022-09-22 23:16:40 +0900

    Backjoon_1110

---
+++ src/Backjoon_1110.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Backjoon_1110 {
+    public static void main(String[] args) throws Exception{
+        Scanner sc=new Scanner(System.in);
+        int result=-1, num=sc.nextInt(),num2=num,count=0;
+
+        for(;result!=num;count++){
+            result=(num2%10*10)+(num2/10+num2%10)%10;
+            num2=result;
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
